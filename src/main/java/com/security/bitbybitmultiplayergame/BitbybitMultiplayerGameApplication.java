bacbf2a2fdcc8fba595c9fd9d581cc9b680a952b
sumitverma77 <987sumitverma@gmail.com>
2025-01-30 00:20:20 +0530

    Websocket Intigration with Rest Controller

--- src/main/java/com/security/bitbybitmultiplayergame/BitbybitMultiplayerGameApplication.java
+++ src/main/java/com/security/bitbybitmultiplayergame/BitbybitMultiplayerGameApplication.java
@@ -2,8 +2,10 @@ package com.security.bitbybitmultiplayergame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.security.bitbybitmultiplayergame", "handler"})
 public class BitbybitMultiplayerGameApplication {
 
     public static void main(String[] args) {
