bd80b7232ae741871055c860c95502e9059772d0
sumitverma77 <987sumitverma@gmail.com>
2025-01-29 10:52:44 +0530

    web socket setup

---
+++ src/test/java/com/security/bitbybitmultiplayergame/BitbybitMultiplayerGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.bitbybitmultiplayergame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitbybitMultiplayerGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
